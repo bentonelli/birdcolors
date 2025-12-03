bc93bfe102b3a2feabbebd65358806eeda176cc4
Ben Tonelli <49177019+bentonelli@users.noreply.github.com>
2025-12-03 14:49:13 -0500

    Added divergent color palette, more palettes
    Updated functions to allow 3-color divergent palettes, added contributed palettes, leaderboard function.

--- R/base_colors.R
+++ R/base_colors.R
@@ -12,12 +12,17 @@ bird_palettes <- list(
   Cassin_s_Finch = c("#DBD9DE","#C63144"),
   Barn_Swallow = c("#4B8EBB","#D89339"),
 
-  # 3 color - best for divergent color palettes
+  # 3 color - used for divergent or discrete color palettes
   Lazuli_Bunting = c("#BB6E31","#95A0A6","#0086BF"),
   Allen_s_Hummingbird = c("#CC5A4F","#61555D","#809D89"),
   Thick_billed_Euphonia = c("#375D88","#62605E","#F3D048"),
   Yellow_headed_Amazon = c("#425013","#F3CA56","#A94843"),
-  Costa_s_Hummingbird = c("#737A54","#948873","#7E2786"),
+  Costa_s_Hummingbird = c("#537236","#BFB19D","#761B70"),
+  Andean_Cock_of_the_Rock = c("#DD5426","#7D7790","#0D0A06"),
+  Bluethroat = c("#EE9C62","#E0D5D6","#68A9F0"),
+  Lovely_Sunbird = c("#305A83","#ECE7A8","#880C07"),
+  Curl_crested_Jay = c("#5B413C","#BAB9B5","#192258"),
+  Northern_Shoveler = c("#AE512B","#000308","#4FA465"),
 
   # 4 color + palettes - best for discrete color palettes
 
@@ -25,13 +30,16 @@ bird_palettes <- list(
   Belted_Kingfisher = c("#08070C","#6F82A1","#8E4517","#FBFBFD"),
   Gray_crowned_Rosy_finch = c("#F7DEDB","#7B6150","#C0CAD7","#1C1F20"),
   Elegant_Euphonia = c("#C0AF51","#568DCB","#554445","#993C0E"),
+  Black_spotted_Bare_eye = c("#D05143","#5B2610","#090B0A","#DDC691"),
 
   # 5 color
-  Paradise_Tanager = c("#00B8DA","#FF4A31","#B0D301","#173076","#000001"),
+  Paradise_Tanager = c("#B6F54D","#496FDE","#E83224","#64D7FB","#030504"),
+  Hairy_crested_Antbird = c("#C79365","#CBF1FB","#4D2319","#E2D3BC","#080A0A"),
 
   # 6 color
   European_Goldfinch = c("#1B2025","#FCF369","#DDBF98","#980F1B","#B38759","#F4F4E9"),
   Black_backed_Dwarf_Kingfisher = c("#3F3E62","#1020AF","#902BD2","#CA5988","#E39F39","#BE3F1F"),
+  Blue_Pitta = c("#2D43AB","#EFCC48","#E2682A","#D4301F","#866929","#070708"),
 
   # 7 color
   Scarlet_Macaw = c("#FF3D3F","#3870C5","#E0AD04","#262A31","#B8CBDE","#33794A","#273C93"),
@@ -90,33 +98,62 @@ bird_colors <- function(palette_name="Scarlet Macaw", ncols = NA, reverse = FALS
 #' Create ggplot color gradient
 #'
 #' @param bird_cols Bird colors extracted via bird_colors function
+#' @param midpoint Midpoint of diverging color palette, only for 3-color palettes
 #'
 #' @returns ggplot-friendly color gradient
 #' @export
 #'
 #' @examples scale_color_bird(bird_colors())
 #'
-scale_color_bird <- function(bird_cols){
+scale_color_bird <- function(bird_cols,midpoint=NA){
+  
   colpal <- grDevices::colorRampPalette(bird_cols, space = "Lab")
-  ggplot2::scale_colour_gradientn(
-    colours = colpal(300)
-  )
+  
+  if(!is.na(midpoint) & length(bird_cols)==3){
+    
+    ggplot2::scale_color_gradient2(
+      low = bird_cols[1],mid=bird_cols[2],high=bird_cols[3],
+      midpoint=midpoint)
+    
+  } else {
+    
+    if(!is.na(midpoint)){
+      warning("Midpoint not supported when number of colors != 3")
+    }
+    
+    ggplot2::scale_colour_gradientn(
+      colours = colpal(300))
+  }
 }
 
 #' Create ggplot fill gradient
 #'
 #' @param bird_cols Bird colors extracted via bird_colors function
+#' @param midpoint Midpoint of diverging color palette, only for 3-color palettes
 #'
 #' @returns ggplot-friendly fill gradient
 #' @export
 #'
 #' @examples scale_fill_bird(bird_colors())
 #'
-scale_fill_bird <- function(bird_cols){
+scale_fill_bird <- function(bird_cols,midpoint=NA){
+  
   colpal <- grDevices::colorRampPalette(bird_cols, space = "Lab")
-  ggplot2::scale_fill_gradientn(
-    colours = colpal(300)
-  )
+  
+  if(!is.na(midpoint) & length(bird_cols)==3){
+    
+    ggplot2::scale_fill_gradient2(
+      low = bird_cols[1],mid=bird_cols[2],high=bird_cols[3],
+      midpoint=midpoint)
+    
+  } else {
+    
+    if(!is.na(midpoint)){
+      warning("Midpoint not supported when number of colors != 3")
+    }
+    ggplot2::scale_fill_gradientn(
+      colours = colpal(300))
+  }
 }
 
 #Print out available birds
@@ -130,4 +167,17 @@ bird_menu <- function(){
   palette_options <- data.frame(bird_names = names(bird_palettes),
                                 ncols=as.numeric(lapply(bird_palettes,length)))
   print(palette_options)
+}
+
+#Print out leaderboard
+#' Print out leaders in contributing bird color palettes
+#'
+#' @returns Prints data.frame of leaderboard in contributing bird color palettes
+#' @export
+#'
+#' @examples leaderboard()
+leaderboard <- function(){
+  leaderboard_bc <- data.frame(people = c("Ben Tonelli","Chris Sayers"),
+                                contributions = c(24,5))
+  print(leaderboard_bc)
 }
